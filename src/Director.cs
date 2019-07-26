e767afe4e3404519556039a2eb18880405ba1be4
Acid Bubbles <acidbubbles.anon@gmail.com>
2019-07-26 00:00:26 -0400

    Allow attaching a window camera for easier scene creation

--- src/Director.cs
+++ src/Director.cs
@@ -16,9 +16,13 @@ public class Director : MVRScript
     private float _previousPlayerHeight;
     private Quaternion _previousRotation;
     private bool _active;
+    private bool _windowCameraActive;
     private AnimationPattern _pattern;
     private UserPreferences _preferences;
-    private AnimationStep _currentStep;
+    private AnimationStep _lastStep;
+    private JSONStorableBool _attachWindowCameraJSON;
+    private Atom _windowCamera;
+    private FreeControllerV3 _windowCameraController;
 
     public override void Init()
     {
@@ -40,6 +44,12 @@ public class Director : MVRScript
         _activeJSON = new JSONStorableBool("Active", false);
         RegisterBool(_activeJSON);
         var activeToggle = CreateToggle(_activeJSON, false);
+
+        // TODO: Changing this while active can screw up things.
+        _attachWindowCameraJSON = new JSONStorableBool("WindowCamera", false);
+        RegisterBool(_attachWindowCameraJSON);
+        var attachWindowCameraToggle = CreateToggle(_attachWindowCameraJSON, false);
+        attachWindowCameraToggle.label = "Attach WindowCamera (setup)";
     }
 
     public void Update()
@@ -63,17 +73,17 @@ public class Director : MVRScript
                 _active = true;
             }
 
+            // NOTE: activeStep is protected for some reason
+            var currentStep = _pattern.steps.FirstOrDefault(step => step.active);
+
             if (_active)
             {
-                // NOTE: activeStep is protected for some reason
-                var target = _pattern.steps.FirstOrDefault(step => step.active);
-
-                if (_currentStep != target)
+                if (_lastStep != currentStep)
                 {
-                    _currentStep = target;
+                    _lastStep = currentStep;
 
                     {
-                        var navigationRigRotation = target.transform.rotation;
+                        var navigationRigRotation = currentStep.transform.rotation;
                         if (!superController.MonitorRig.gameObject.activeSelf)
                             navigationRigRotation *= Quaternion.Euler(0, navigationRig.eulerAngles.y - _possessor.transform.eulerAngles.y, 0f);
                         navigationRig.rotation = navigationRigRotation;
@@ -81,13 +91,36 @@ public class Director : MVRScript
 
                     {
                         var up = navigationRig.up;
-                        var positionOffset = navigationRig.position + target.transform.position - _possessor.autoSnapPoint.position;
+                        var positionOffset = navigationRig.position + currentStep.transform.position - _possessor.autoSnapPoint.position;
                         // Adjust the player height so the user can adjust as needed
                         var playerHeightAdjustOffset = Vector3.Dot(positionOffset - navigationRig.position, up);
                         navigationRig.position = positionOffset + up * -playerHeightAdjustOffset;
                         superController.playerHeightAdjust += playerHeightAdjustOffset;
                     }
                 }
+            }
+
+            if (_attachWindowCameraJSON.val)
+            {
+                if (_windowCamera == null)
+                {
+                    _windowCamera = superController.GetAtomByUid("WindowCamera");
+                    if (_windowCamera == null)
+                    {
+                        SuperController.LogError("There is no WindowCamera atom in the scene, maybe you deleted or renamed it?");
+                        _attachWindowCameraJSON.val = false;
+                        return;
+                    }
+                    _windowCamera.GetBoolJSONParam("on").val = true;
+                    _windowCameraController = _windowCamera.freeControllers[0];
+                    _windowCameraController.canGrabPosition = false;
+                    _windowCameraController.canGrabRotation = false;
+                }
+                _windowCameraController.transform.SetPositionAndRotation(currentStep.transform.position, currentStep.transform.rotation);
+            }
+            else if (_windowCamera != null)
+            {
+                RestoreWindowCamera();
             }
         }
         catch (Exception e)
@@ -97,9 +130,26 @@ public class Director : MVRScript
         }
     }
 
+    private void RestoreWindowCamera()
+    {
+        if (_windowCamera)
+        {
+            _windowCamera.GetBoolJSONParam("on").val = false;
+            _windowCamera = null;
+        }
+
+        if (_windowCameraController)
+        {
+            _windowCameraController.canGrabPosition = true;
+            _windowCameraController.canGrabRotation = true;
+            _windowCameraController = null;
+        }
+    }
+
     public void OnDisable()
     {
         Restore();
+        RestoreWindowCamera();
     }
 
     private void Restore()
@@ -107,10 +157,11 @@ public class Director : MVRScript
         if (!_active)
             return;
 
-        _currentStep = null;
+        _active = false;
+        _lastStep = null;
+
         SuperController.singleton.navigationRig.rotation = _previousRotation;
         SuperController.singleton.navigationRig.position = _previousPosition;
         SuperController.singleton.playerHeightAdjust = _previousPlayerHeight;
-        _active = false;
     }
 }
